3cd16133b5672b5f0acdd711758b6e583cb0b1ef
f16falcona46 <li07.jason@gmail.com>
2016-08-19 00:56:17 -0400

    test of GDI

--- wxLua/test_window.lua
+++ wxLua/test_window.lua
@@ -10,17 +10,6 @@ function initialize_threads(context)
 	context.threadpool = {}
 	context.currentthread = 1
 	
-	local tasks = {}
-	for i=1,10 do
-		tasks[i] = function ()
-				for j=1,3*i*i do
-					coroutine.yield()
-				end
-				context.progressbar:SetValue(context.progressbar:GetValue() + 1)
-			end
-	end
-	
-	
 	context.mainpanel:Connect(wx.wxID_ANY, wx.wxEVT_IDLE,
 		function (event)
 			for i=#context.threadpool,1,-1 do
@@ -35,11 +24,6 @@ function initialize_threads(context)
 				coroutine.resume(context.threadpool[context.currentthread])
 				context.currentthread = context.currentthread + 1
 			end
-		end)
-	
-	create_task(context, tasks, function ()
-			context.threadindicator:SetLabel("Threads stopped")
-			wx.wxMessageBox("Done!", "title", wx.wxOK + wx.wxICON_INFORMATION)
 		end)
 end
 
@@ -75,10 +59,11 @@ function create_window()
 		Button3 = 3,
 		Button4 = 4,
 		Button5 = 5,
-		Timer1 = 6
+		Timer1 = 6,
+		Panel1 = 7
 	}
 	context.frame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "Test", wx.wxDefaultPosition, wx.wxSize(600, 480), wx.wxDEFAULT_FRAME_STYLE)
-	context.frame:SetMinSize(wx.wxSize(400, 300))
+	context.frame:SetMinSize(wx.wxSize(500, 300))
 	context.mainpanel = wx.wxPanel(context.frame, wx.wxID_ANY)
 	--context.frame:SetBackgroundColour(wx.wxSystemSettings.GetColour(wx.wxSYS_COLOUR_WINDOW))
 	
@@ -94,10 +79,12 @@ function create_window()
 	context.buttoncounter = wx.wxStaticText(context.mainpanel, -1, "Button clicks: 0")
 	context.sizer:Add(context.buttoncounter, wx.wxGBPosition(1, 2), wx.wxGBSpan(), wx.wxALL + wx.wxALIGN_CENTER, 5)
 	context.sizer:Add(wx.wxButton(context.mainpanel, ID.Button5, "MultiSpan", wx.wxDefaultPosition, wx.wxDefaultSize, 0), wx.wxGBPosition(0, 3), wx.wxGBSpan(3, 1), wx.wxALL + wx.wxEXPAND, 5)
+	context.drawpanel = wx.wxPanel(context.mainpanel, ID.Panel1, wx.wxDefaultPosition, wx.wxSize(100, 100))
+	context.sizer:Add(context.drawpanel, wx.wxGBPosition(0, 4), wx.wxGBSpan(), wx.wxALL + wx.wxEXPAND, 5)
 	context.threadindicator = wx.wxStaticText(context.mainpanel, -1, "Threads running")
 	context.sizer:Add(context.threadindicator, wx.wxGBPosition(2, 0), wx.wxGBSpan(1, 3), wx.wxALL + wx.wxEXPAND, 5)
 	context.progressbar = wx.wxGauge(context.mainpanel, wx.wxID_ANY, 10)
-	context.sizer:Add(context.progressbar, wx.wxGBPosition(3, 0), wx.wxGBSpan(1, 4), wx.wxALL + wx.wxEXPAND, 5)
+	context.sizer:Add(context.progressbar, wx.wxGBPosition(3, 0), wx.wxGBSpan(1, 5), wx.wxALL + wx.wxEXPAND, 5)
 	
 	
 	context.sizer:AddGrowableRow(0)
@@ -122,8 +109,29 @@ function create_window()
 			context.sizer:Layout()
 		end, {i = 0}))
 	context.mainpanel:Connect(ID.Button5, wx.wxEVT_COMMAND_BUTTON_CLICKED, function (event) wx.wxMessageBox("Multispan clicked", "Title", wx.wxOK + wx.wxICON_INFORMATION) end)
+	context.drawpanel:Connect(ID.Panel1, wx.wxEVT_PAINT,
+		create_closure(function (data, event)
+			dc = wx.wxPaintDC(data.drawpanel)
+			dc:DrawLine(0, 0, 100, 100)
+			dc:delete()
+		end, { drawpanel = context.drawpanel }))
 	
 	initialize_threads(context)
+	
+	local tasks = {}
+	for i=1,10 do
+		tasks[i] = function ()
+				for j=1,3*i*i do
+					coroutine.yield()
+				end
+				context.progressbar:SetValue(context.progressbar:GetValue() + 1)
+			end
+	end
+	
+	create_task(context, tasks, function ()
+			context.threadindicator:SetLabel("Threads stopped")
+			wx.wxMessageBox("Done!", "title", wx.wxOK + wx.wxICON_INFORMATION)
+		end)
 	
 	context.timer:Start(1000)
 	context.mainpanel:SetSizer(context.sizer)
